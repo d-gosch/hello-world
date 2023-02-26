036d2099a19457fd141b865b2250156f87fec946
Gosch <dmeyer.private@gmail.com>
2023-02-26 04:24:12 +0100

    NR-1: add tailwind support + new component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
+import { reactive } from 'vue';
 import { RouterLink, RouterView } from 'vue-router'
 import PokemonApi from './components/PokemonApi.vue'
+import Tile from './components/Tile.vue';
+const tiles = reactive([
+  { title: 'Pokémon', description: 'Messing around with pokeapi' },
+  { title: 'test', description: 'duno' },
+  { title: 'foo', description: 'Todo' },
+  { title: 'bar', description: 'i dont know' }])
 </script>
 
 <template>
   <p>Hello World!</p>
   <PokemonApi RouterView="pokemon" />
+  <Tile v-for="tile in tiles" :title="tile.title" :description="tile.description" />
 </template>
 
 <style scoped></style>
---
+++ src/components/Tile.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const props = defineProps({
+    title: String,
+    description: String
+})
+</script>
+<template>
+    <div class="bg-green-400 text-red-900 font-bold">
+        <h3>{{ title }}</h3>
+        <h4>{{ description }}</h4>
+    </div>
+</template>
+<style></style>
