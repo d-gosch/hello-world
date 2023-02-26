da638193b61d69dbf8a11b4ca5b5b30f2d960acd
Gosch <dmeyer.private@gmail.com>
2023-02-26 18:16:24 +0100

    NR-1: tidy up and style

--- src/components/PokemonApi.vue
+++ src/components/PokemonApi.vue
@@ -16,16 +16,20 @@ async function getPokemonName() {
 }
 </script>
 <template>
-    <button @click="getPokemonName">Get Pokemon</button>
-    <input type="text" placeholder="enter Pokemon name" v-model="name">
-    <div v-if="!pokemon">loading</div>
-    <div v-else>
-        <p>{{ pokemon.name }}</p>
-        <img :src="pokemon.sprites.front_shiny_female ?? pokemon.sprites.front_shiny ?? undefined" alt="">
+    <div class="grid grid-cols-5 gap-4">
+        <input
+            class="col-span-4 block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50
+                                                                            focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
+                                                                            dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            @keypress.enter="getPokemonName" type="text" placeholder="Enter Pokémon name" v-model="name">
+        <button
+            class="p-2.5 ml-2 text-sm font-medium text-white bg-green-700 rounded-lg border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
+            @click="getPokemonName">Get Pokemon</button>
+        <div v-if="!pokemon">loading</div>
+        <div v-else>
+            <p>{{ pokemon.name }}</p>
+            <img :src="pokemon.sprites.front_shiny_female ?? pokemon.sprites.front_shiny ?? undefined" alt="">
+        </div>
     </div>
 </template>
-<style>
-p {
-    color: aliceblue;
-}
-</style>
+<style></style>
--- src/views/TileView.vue
+++ src/views/TileView.vue
@@ -3,17 +3,13 @@ import Tile from "../components/Tile.vue"
 import { reactive } from 'vue'
 const tiles = reactive([
     { title: 'Pokémon', description: 'Messing around with pokeapi', routerLink: '/pokemon' },
-    { title: 'test', description: 'duno', routerLink: '/' },
-    { title: 'foo', description: 'Todo', routerLink: '/' },
-    { title: 'bar', description: 'i dont know', routerLink: '/' }])
+])
 </script>
 <template>
-    <div class="w-full">
-        <div class="grid grid-cols-3 gap-4">
-            <Tile
-                class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
-                v-for="tile in tiles" :key="tile.title" :router-link="tile.routerLink" :title="tile.title"
-                :description="tile.description" />
-        </div>
+    <div class="w-full grid grid-cols-3 gap-4">
+        <Tile
+            class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            v-for="tile in tiles" :key="tile.title" :router-link="tile.routerLink" :title="tile.title"
+            :description="tile.description" />
     </div>
 </template>
