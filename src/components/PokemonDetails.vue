db5f454bcdee16b53bd944aade76e95c44aff5c1
Gosch <dmeyer.private@gmail.com>
2023-03-14 18:56:56 +0100

    fix: directly fetching the data

--- src/components/PokemonDetails.vue
+++ src/components/PokemonDetails.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { pokemonStore } from '@/stores/counter';
-import { PokemonClient, type LocationAreaEncounter, type Type } from 'pokenode-ts';
-import { computed, ref, watch } from 'vue';
+import { LocationClient, PokemonClient, type LocationArea, type LocationAreaEncounter, type Type } from 'pokenode-ts';
+import { computed, onMounted, ref, watch } from 'vue';
 
 import PokemonEncounterLocations from './PokemonEncounterLocations.vue';
 import LocationDetails from './LocationDetails.vue';
@@ -9,6 +9,8 @@ import LocationDetails from './LocationDetails.vue';
 const location = ref('')
 const encounterLocations = ref<LocationAreaEncounter[] | undefined>()
 const api = new PokemonClient({ cacheOptions: { maxAge: 5000, exclude: { query: false } } })
+const apiLocationClient = new LocationClient({ cacheOptions: { maxAge: 5000, exclude: { query: false } } })
+const locations = ref<LocationArea[] | undefined>(undefined)
 const showEncounterDetail = ref(false)
 const toggleEncounterDetail = computed(() => {
     return showEncounterDetail.value
@@ -17,13 +19,17 @@ const toggleEncounterDetail = computed(() => {
 })
 async function getLocation() {
     pokemonStore.value ? await api.getPokemonLocationAreaById(pokemonStore.value.id)
-        .then((data) => encounterLocations.value = data)
+        .then(async (data) => {
+            locations.value = await Promise.all(data.map((location) => apiLocationClient.getLocationAreaByName(location.location_area.name)))
+        })
         .catch((error) => console.log(error)) : 'No Location yet!'
 }
 
 
 watch(pokemonStore, getLocation)
-
+onMounted(() => {
+    getLocation()
+})
 
 
 </script>
@@ -33,7 +39,7 @@ watch(pokemonStore, getLocation)
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> #{{ pokemonStore.id }} {{
                 pokemonStore.name }}</h5>
 
-            <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
+            <img class=" object-contain w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
                 :src="pokemonStore.sprites.front_default ?? undefined" :alt="pokemonStore.name">
 
             <div class="flex flex-col justify-between p-4 leading-normal">
@@ -48,24 +54,25 @@ watch(pokemonStore, getLocation)
                     <button class="text-lg underline font-bold"
                         @click="showEncounterDetail = !showEncounterDetail">Encounter
                         location:</button>
-                    <div v-if="toggleEncounterDetail" class="grid grid-cols-3 gap-4">
-                        <div v-if="encounterLocations?.length">
-                            <PokemonEncounterLocations @click="location = encounterLocation.location_area.name"
-                                v-for="encounterLocation in encounterLocations" :key="encounterLocation.location_area.name"
-                                :location-name="encounterLocation.location_area.name" />
-                        </div>
+                    <div v-if="toggleEncounterDetail" class="flex flex-col items-start">
+                        <PokemonEncounterLocations @click="location = encounterLocation.name" v-if="locations?.length"
+                            v-for="encounterLocation in locations" :key="encounterLocation.id"
+                            :location="encounterLocation" />
                         <div v-else>No locations</div>
                     </div>
                 </div>
                 <div class="text-sm font-medium text-gray-900 dark:text-white">
                     <p class="text-lg underline font-bold">Base Stats:</p>
-                    <div class="col-span-2 table-auto">
-                        <tbody>
-                            <tr v-for="baseStat in pokemonStore.stats">
-                                <td>{{ baseStat.stat.name }}</td>
-                                <td>{{ baseStat.base_stat }}</td>
-                            </tr>
-                        </tbody>
+                    <div class="col-span-2">
+
+                        <ul v-for="baseStat in pokemonStore.stats">
+                            <li>
+                                <div class="flex justify-between"><span>{{ baseStat.stat.name }}</span>
+                                    <span>{{ baseStat.base_stat }}</span>
+                                </div>
+                            </li>
+                        </ul>
+
                     </div>
                 </div>
                 <div class="text-sm font-medium text-gray-900 dark:text-white">
