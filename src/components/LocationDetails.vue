5f8ab2c34ae940d67a3aba8a37e39ee9624b33e4
Gosch <dmeyer.private@gmail.com>
2023-04-24 20:40:42 +0200

    feat: adding explicit element for expanding and collapsing

--- src/components/LocationDetails.vue
+++ src/components/LocationDetails.vue
@@ -7,7 +7,8 @@ const emit = defineEmits(['location'])
     <div>
         <h1 class="text-3xl dark:text-white">Location Details Page</h1>
         <div>{{ props.location }}</div>
-        <button class="bg-gray-500 hover:bg-gray-700 text-black text-xl py-2 px-4 rounded-full"
+        <button
+            class="py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
             @click="emit('location', null)">close</button>
     </div>
 </template>
