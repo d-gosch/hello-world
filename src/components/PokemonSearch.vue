0811a1cd6199852289e635733031a515280cc5b1
Gosch <dmeyer.private@gmail.com>
2023-04-27 00:08:39 +0200

    fix: routing behaviour

--- src/components/PokemonSearch.vue
+++ src/components/PokemonSearch.vue
@@ -20,25 +20,24 @@ onMounted(() => {
     fetchPokemonList() // Load all pokemon names with reference
 })
 function submit() {
-    router.push(`pokemon/${name.value}`)
+    router.push(`/pokemon/${name.value}`)
 }
 </script>
 
 <template>
-    <div class="grid grid-cols-12 gap-4">
+    <form class="flex gap-4" @submit.prevent="submit">
         <input
-            class=" text-zinc-50 col-span-10 block w-full p-4 pl-10 text-sm border border-gray-300 rounded-lg bg-gray-50
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+            class="text-zinc-50 block w-full p-4 pl-10 text-sm border border-gray-300 rounded-lg bg-gray-50
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
             list="pokemonNameData" @keypress.enter="submit" v-model="name">
         <datalist id="pokemonNameData">
             <option v-for="pokemonName in pokemonNameList" :key="pokemonName" :value="pokemonName" />
         </datalist>
 
-        <router-link :to="name"
-            class="col-span-2 text-center p-4 text-sm font-medium text-white bg-green-700 rounded-lg border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Get
-            Pokemon</router-link>
+        <button
+            class="text-center p-4 text-sm font-medium text-white bg-green-700 rounded-lg border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Get
+            Pokemon</button>
 
-
-    </div>
+    </form>
 </template>
 <style></style>
--- src/components/PokemonTypeButton.vue
+++ src/components/PokemonTypeButton.vue
@@ -7,8 +7,7 @@ const props = defineProps({
 })
 </script>
 <template>
-    <router-link to="pokemonType"
-        class="p-1 m-2 dark:hover:bg-green-700 dark:hover:text-white dark:hover:rounded-lg dark:rounded-lg">
+    <router-link to="pokemonType" class="p-1 m-2 hover:bg-green-700 hover:text-white rounded-lg">
         <span>{{ pokemonType.type.name }}</span>
 
     </router-link>
