dac7494039597773c6a2377b641de3c9cd678a2a
Gosch <dmeyer.private@gmail.com>
2023-04-26 20:13:54 +0200

    feat: introduce router links

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-import { reactive } from 'vue';
-import { RouterLink, RouterView } from 'vue-router'
-import PokemonApi from './components/PokemonApi.vue'
-import Tile from './components/Tile.vue';
+import { RouterView } from 'vue-router'
 
 </script>
 
